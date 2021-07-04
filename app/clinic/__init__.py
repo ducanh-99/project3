d487beb367040b675f9c6c9fc210e7f22364a74b
ducanh-99 <anh.nd1@teko.vn>
2021-07-04 21:25:36 +0700

    add base clinic

--- app/clinic/__init__.py
+++ app/clinic/__init__.py
@@ -1,2 +1,25 @@
+from . import cardiology, dentistry, dermatology, ear_nose_throat, gastroenterology, neurology, nutrition, oncology, ophthalmology,  urology
 from .cardiology import Cardiology
-list_clinic = [Cardiology]
+from .dentistry import Dentistry
+from .dermatology import Dermatology
+from .ear_nose_throat import EarNoseThroat
+from .gastroenterology import Gastroenterology
+from .neurology import Neurology
+from .nutrition import Nutrition
+from .oncology import Oncology
+from .ophthalmology import Ophthalmology
+from .urology import Urology
+
+# cardiology
+# dentistry
+# dermatology
+# ear_nose_throat
+# gastroenterology
+# neurology
+# nutrition
+# oncology
+# ophthalmology
+# urology
+
+list_clinic = [Cardiology(), Dentistry(), Dermatology(), EarNoseThroat(),
+               Gastroenterology(), Neurology(), Nutrition(), Oncology(), Ophthalmology(), Urology()]
---
+++ app/clinic/dentistry.py
@@ -0,0 +1,9 @@
+from .clinic_base import ClinicBase
+from app.core.singleton import Singleton
+
+
+class Dentistry(Singleton, ClinicBase):
+    id_clinic = 1
+
+    def __init__(self) -> None:
+        super().__init__()
